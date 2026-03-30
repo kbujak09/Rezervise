dd068c768d4979a2687bf1a7193d81c347a0a679
kbujak09 <kaccpper2@gmail.com>
2026-03-30 02:26:38 +0200

    Search bar is focused by default

--- client/src/features/LandingPage/components/Hero/SearchBarMobile/SearchBarMobile.tsx
+++ client/src/features/LandingPage/components/Hero/SearchBarMobile/SearchBarMobile.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useRef } from 'react';
+
 import closeIcon from '../../../../../assets/close_icon.svg';
 
 import styles from './SearchBarMobile.module.scss';
 
 export default function SearchBarMobile({closeMenu}: {closeMenu: () => void}) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <input 
           type="text" 
           className={styles.searchbar}
+          ref={inputRef}
         />
         <img 
           src={closeIcon} 
