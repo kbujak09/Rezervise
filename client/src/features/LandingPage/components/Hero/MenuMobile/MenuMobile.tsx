76153553a3bd7776c1c16bfcaf7d8354ff26ed1b
kbujak09 <kaccpper2@gmail.com>
2026-04-05 00:32:28 +0200

    Basic Mobile Menu

--- client/src/features/LandingPage/components/Hero/MenuMobile/MenuMobile.tsx
+++ client/src/features/LandingPage/components/Hero/MenuMobile/MenuMobile.tsx
@@ -1,3 +1,5 @@
+import closeIcon from '../../../../../assets/close_icon.svg';
+
 import styles from './MenuMobile.module.scss';
 
 interface MenuMobileProps {
@@ -8,7 +10,10 @@ interface MenuMobileProps {
 export default function MenuMobile({isOpen, closeMenu}: MenuMobileProps) {
   return (
     <div className={`${styles.container} ${isOpen ? styles.open : null}`} >
-      <div onClick={closeMenu}>WYJDZ</div>
+      <div className={styles.header}>
+        <img className={styles.closeButton} onClick={closeMenu} src={closeIcon} alt="zamknij menu" />
+        <div className={styles.logo}>REZERVISE</div>
+      </div>
     </div>
   )
 }
